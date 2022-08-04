efdd1627b21605de9ef5ea35e92e6f5a989639e8
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-08-04 23:45:08 +0400

    Delete Fibonacci.py

--- Fibonacci.py
+++
@@ -1,8 +0,0 @@
-def fibonacci_recursive(n):
-    #Find nth member in sequence of fibonacci
-
-    if n <= 1:
-        return n
-    return (fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2))
-n = int(input("Input a number: "))
-print(f"Fibonacci's {n}th member is {fibonacci_recursive(n)}")
