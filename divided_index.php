77350237d354ee6ccc6c093bae7fc0cb29cc910b
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-08-04 23:48:42 +0400

    Delete divided_index.php

--- divided_index.php
+++
@@ -1,9 +0,0 @@
-def divided_index(lst):
-    #Find the count of numbers, that are divided their index without remainder
-
-    count = 0
-    for i in range(1, len(lst)):
-        if lst[i] % i == 0:
-            count += 1
-    return count
-print("Count =",divided_index([1, 2, 4, 15, 23]))
