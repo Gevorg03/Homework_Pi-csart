3ecbf136bb8ced5e9d285375df1049160103f58f
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-08-05 17:15:49 +0400

    Create digit_sum_multiplication.py

---
+++ practical_work/digit_sum_multiplication.py
@@ -0,0 +1,15 @@
+"""Find the differene of digit's sum and digit's multiplication"""
+
+def digit(num):
+    sum = 0
+    multiple = 1
+    while num != 0:
+        n = num % 10
+        sum += n
+        multiple *= n
+        num //= 10
+    return multiple - sum
+
+
+num = int(input("Input a number: "))
+print("Result =", digit(num))
