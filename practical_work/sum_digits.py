73c4b9d67dd45c550f45824b9b44aefe892e16cb
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-08-05 17:14:06 +0400

    Create sum_digits.py

---
+++ practical_work/sum_digits.py
@@ -0,0 +1,12 @@
+"""Find the sum of number's digits"""
+
+The first solution
+def get_sum(num):
+    sum = 0
+    while num != 0:
+        n = num % 10
+        sum += n
+        num //= 10
+    return sum
+num = int(input("Input a number: "))
+print("Sum =", get_sum(num))
