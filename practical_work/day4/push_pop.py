e4f88876fb212f5266064dfa7ffd3d249022f63b
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-08-08 22:04:37 +0400

    Update push_pop.py

---
+++ practical_work/day4/push_pop.py
@@ -0,0 +1,12 @@
+def push_pop(target: list, n):
+    lst = []
+    for i in range(1, target[-1] + 1):
+        if i in target:
+            lst.append("push")
+        else:
+            lst.append("push")
+            lst.append("pop")
+    return lst
+
+
+print(push_pop([1, 3], 3))
