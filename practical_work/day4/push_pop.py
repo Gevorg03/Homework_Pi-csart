4ed2d3f45a2791de33a468b792c2855f48e452d5
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-08-25 12:46:10 +0400

    Update push_pop.py

--- practical_work/day4/push_pop.py
+++ practical_work/day4/push_pop.py
@@ -1,12 +1,10 @@
-def push_pop(target: list, n):
-    lst = []
-    for i in range(target[0], target[-1] + 1):
-        if i in target:
-            lst.append("push")
-        else:
-            lst.append("push")
-            lst.append("pop")
-    return lst
-
-
-print(push_pop([1, 3], 3))
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        lst = []
+        for i in range(1, target[-1] + 1):
+            if i in target:
+                lst.append("Push")
+            else:
+                lst.append("Push")
+                lst.append("Pop")
+        return lst
